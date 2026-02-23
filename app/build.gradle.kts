87bcfaf524c216a217799fc7c2cf49d3fac91e8b
João Vítor Agustini <thebooking13@gmail.com>
2026-02-23 10:43:19 -0400

    Add Material Components for Material3 theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.05.00")
     implementation(composeBom)
     androidTestImplementation(composeBom)
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
